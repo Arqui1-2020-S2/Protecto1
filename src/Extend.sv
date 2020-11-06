e23743265af217be9fb264769b6cce40268d7350
Arturo Cordoba <arturocv16@gmail.com>
2020-11-05 20:46:54 -0600

    Update Extend Module

--- src/Extend.sv
+++ src/Extend.sv
@@ -1,5 +1,5 @@
 module Extend(input  logic[1:0]  ImmSrc,
-				  input  logic[17:0] Instr,
+				  input  logic[19:0] Instr,
 				  output logic[31:0] ExtImm);
 // ImmSrc: selector de operacion
 // Instr: bits de la instruccion que corresponden al inmediato
@@ -7,11 +7,11 @@ module Extend(input  logic[1:0]  ImmSrc,
 always_comb begin
 	case (ImmSrc)
 				 // Extension de ceros
-		2'b00: ExtImm = {19'b0, Instr[12:0]};
+		2'b00: ExtImm = {19'b0, Instr[15:0]};
 				 // Extension de signo
-		2'b01: ExtImm = {{19{Instr[12]}},Instr[12:0]};
+		2'b01: ExtImm = {{16{Instr[15]}},Instr[15:0]};
 				 // Extension de signo con dos corrimientos a la izquierda
-		2'b10: ExtImm = {{12{Instr[17]}},Instr[17:0], 2'b00};
+		2'b10: ExtImm = {{10{Instr[19]}},Instr[19:0], 2'b00};
 		default: ExtImm = 32'bx; // Indefinido
 	endcase
 end
--- src/Extend_tb.sv
+++ src/Extend_tb.sv
@@ -1,7 +1,7 @@
 module Extend_tb();
 
 logic[1:0]  ImmSrc;
-logic[17:0] Instr;
+logic[19:0] Instr;
 logic[31:0] ExtImm;
 
 Extend extend(ImmSrc, Instr, ExtImm);
@@ -9,42 +9,42 @@ Extend extend(ImmSrc, Instr, ExtImm);
 initial begin
 	// Caso 1: Extension de ceros
 	ImmSrc = 2'b00;
-	Instr = 18'b111100001111000011;
+	Instr = 20'b11111100001111000011;
 	#10
-	assert (ExtImm === 32'b00000000000000000000001111000011) else $error("Case 1: ExtImm failed.");
+	assert (ExtImm === 32'b00000000000000001100001111000011) else $error("Case 1: ExtImm failed.");
 	#10;
 	
 	// Caso 2: Extension de ceros
 	ImmSrc = 2'b00;
-	Instr = 18'b111111111111111111;
+	Instr = 20'b11111111111111111111;
 	#10
-	assert (ExtImm === 32'b00000000000000000001111111111111) else $error("Case 2: ExtImm failed.");
+	assert (ExtImm === 32'b00000000000000001111111111111111) else $error("Case 2: ExtImm failed.");
 	#10;
 	
 	// Caso 3: Extension de signo
 	ImmSrc = 2'b01;
-	Instr = 18'b111100001111000011;
+	Instr = 20'b11110100001111000011;
 	#10
-	assert (ExtImm === 32'b00000000000000000000001111000011) else $error("Case 3: ExtImm failed.");
+	assert (ExtImm === 32'b00000000000000000100001111000011) else $error("Case 3: ExtImm failed.");
 	#10;
 	
 	// Caso 4: Extension de signo
 	ImmSrc = 2'b01;
-	Instr = 18'b111111111111111111;
+	Instr = 20'b11111111111111111111;
 	#10
 	assert (ExtImm === 32'b11111111111111111111111111111111) else $error("Case 4: ExtImm failed.");
 	#10;
 	
 	// Caso 5: Extension de signo con corrimiento de 2 a la izquierda
 	ImmSrc = 2'b10;
-	Instr = 18'b001100001111000011;
+	Instr = 20'b00000100001111000011;
 	#10
-	assert (ExtImm === 32'b00000000000000110000111100001100) else $error("Case 5: ExtImm failed.");
+	assert (ExtImm === 32'b00000000000000010000111100001100) else $error("Case 5: ExtImm failed.");
 	#10;
 	
 	// Caso 6: Extension de signo con corrimiento de 2 a la izquierda
 	ImmSrc = 2'b10;
-	Instr = 18'b111111100011100011;
+	Instr = 20'b11111111100011100011;
 	#10
 	assert (ExtImm === 32'b11111111111111111110001110001100) else $error("Case 6: ExtImm failed.");
 	#10;
